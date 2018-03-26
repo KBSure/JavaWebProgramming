e835129ed8e07d92213f24bc394c328994af29d9
KBSure <kbsure.dev@gmail.com>
2018-03-26 15:46:17 +0900

    Client, Server 제작

---
+++ study01/src/main/java/examples/Client.java
@@ -0,0 +1,27 @@
+package examples;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.Buffer;
+
+public class Client {
+    public static void main(String[] args) {
+        try(Socket client = new Socket("10.10.118.199", 8080);){
+            InputStream inputStream = System.in;
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            OutputStream outputStream = client.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream);
+
+            String line = null;
+            while((line = bufferedReader.readLine()) != null){
+                printWriter.println(line);
+                printWriter.flush();
+            }
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+}
--- study01/src/main/java/examples/IOExam04.java
+++ study01/src/main/java/examples/IOExam04.java
@@ -1,8 +1,26 @@
 package examples;
 
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class IOExam04 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        try(FileOutputStream fos = new FileOutputStream("input.txt");) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            FileOutputStream fos = new FileOutputStream("input.txt");
+            String line = null;
+            while((line = br.readLine()) != null){
+                if("quite".equals(line))
+                    break;
+                System.out.println(line);
+            }
+
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
 
     }
 }
