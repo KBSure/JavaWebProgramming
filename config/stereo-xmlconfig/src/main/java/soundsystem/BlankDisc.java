15a22705c367afb24eafa5764113864df51e9bbc
KBSure <kbsure.dev@gmail.com>
2018-04-02 17:03:22 +0900

    JobRunner

--- config/stereo-xmlconfig/src/main/java/soundsystem/BlankDisc.java
+++ config/stereo-xmlconfig/src/main/java/soundsystem/BlankDisc.java
@@ -1,5 +1,8 @@
 package soundsystem;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BlankDisc implements CompactDisc {
 
   private String title;
--- config/stereo-xmlconfig/src/test/java/playsystem/GameTest.java
+++ config/stereo-xmlconfig/src/test/java/playsystem/GameTest.java
@@ -1,15 +1,45 @@
 package playsystem;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/playsystem/*.xml")
 public class GameTest {
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("/playsystem/GameTest-context.xml");
-        Game game = (Game)context.getBean("game");
-        game.play();
+
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+    //System.out으로 출력되는 애들 캡쳐
+    @Autowired
+    Game game;
+
+    @Test
+    public void notNullTest(){
+        Assert.assertNotNull(game);
     }
+
+    @Test
+    public void play(){
+        String expected = "Player1이 던진 주사위가1나왔습니다.\r\n" +
+                "Player2이 던진 주사위가4나왔습니다.\r\n" +
+                "Player3이 던진 주사위가4나왔습니다.\r\n";
+
+        game.play();
+        assertEquals(expected, log.getLog());
+    }
+    //    public static void main(String[] args) {
+//        ApplicationContext context = new ClassPathXmlApplicationContext("/playsystem/GameTest-context.xml");
+//        Game game = (Game)context.getBean("game");
+//        game.play();
+//    }
 }
