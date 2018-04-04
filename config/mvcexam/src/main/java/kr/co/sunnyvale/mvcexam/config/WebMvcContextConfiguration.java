181bfcf91973efcd9e5469e0de4c162607498813
KBSure <kbsure.dev@gmail.com>
2018-04-04 12:03:19 +0900

    mvcexam을 통해 WebMvcContextConfiguration 살펴보기

--- config/mvcexam/src/main/java/kr/co/sunnyvale/mvcexam/config/WebMvcContextConfiguration.java
+++ config/mvcexam/src/main/java/kr/co/sunnyvale/mvcexam/config/WebMvcContextConfiguration.java
@@ -10,12 +10,15 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.util.function.Function;
+
 @Configuration
-@EnableWebMvc
+@EnableWebMvc // WebApplication에 대한 설정은 대부분 자동으로 됨 (스프링 web mvc)
 @ComponentScan(basePackages = { "kr.co.sunnyvale.mvcexam.controller" })
-public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter  { //WMCA는 5.0대에서는 안쓴다. duplicated
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
         registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
         registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
         registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
@@ -40,5 +43,7 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
-    }
+    }//객체가 메모리에 올라는 갔지만 사용은 안 되고 있음
+    //DispaacherServlet이 ViewResolver라는 애들이 있으면 사용한다.
+    //DispatcherServlet이 어떻게 동작하는 지를 알아야 쓰임을 이해할 수 있음.
 }
