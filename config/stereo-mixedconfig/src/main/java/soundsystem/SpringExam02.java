2cc23dc4b4dc87d1516ff989bf437378e7907151
KBSure <kbsure.dev@gmail.com>
2018-04-04 10:53:42 +0900

    mvcexam, mixedconfig import

---
+++ config/stereo-mixedconfig/src/main/java/soundsystem/SpringExam02.java
@@ -0,0 +1,13 @@
+package soundsystem;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class SpringExam02 {
+    public static void main(String[]args) throws Exception{
+        ApplicationContext context = new ClassPathXmlApplicationContext("cdplayer-config.xml");
+
+    }
+}
