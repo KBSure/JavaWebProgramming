67fc03932c77c8805342fd7106b10b602f8eb18d
KBSure <kbsure.dev@gmail.com>
2018-04-08 14:38:02 +0900

    C:/Program Files/Git/ -> /boards 찍히도록

--- helloboard/src/main/java/examples/helloboard/controller/BoardsController.java
+++ helloboard/src/main/java/examples/helloboard/controller/BoardsController.java
@@ -2,40 +2,42 @@ package examples.helloboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/boards")
+@Controller
+@RequestMapping(value = "/boards") //Annotation 문법 공부 필요
 public class BoardsController {
-@GetMapping("/")
+@GetMapping
     public String boardList(){
-        return "/views/boards_list";
+        return "boards_list";
     }
 
-    public String boardRead(){
-        return "/views/boards_read";
-    }
-
-    public String boardWriteForm(){
-        return "/views/boards_write_form";
-    }
-
-    public String boardWrite(){
-        return "redirect:/boards";
-    }
-
-    public String boardDeleteForm(){
-        return "/views/boards_delete_form";
-    }
-
-    public String boardDelete(){
-        return "redirect:/boards";
-    }
-
-    public String boardUpdateForm(){
-        return "/views/boards_update_form";
-    }
-
-    public String boardUpdate(){
-        return "redirect:/boards";
-    }
+//    public String boardRead(){
+//        return "/views/boards_read";
+//    }
+//
+//    public String boardWriteForm(){
+//        return "/views/boards_write_form";
+//    }
+//
+//    public String boardWrite(){
+//        return "redirect:/boards";
+//    }
+//
+//    public String boardDeleteForm(){
+//        return "/views/boards_delete_form";
+//    }
+//
+//    public String boardDelete(){
+//        return "redirect:/boards";
+//    }
+//
+//    public String boardUpdateForm(){
+//        return "/views/boards_update_form";
+//    }
+//
+//    public String boardUpdate(){
+//        return "redirect:/boards";
+//    }
 
 }
