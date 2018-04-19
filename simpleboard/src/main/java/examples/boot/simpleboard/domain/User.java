71d42691c3fb1f76544ddc99a1063210774dfde9
KBSure <kbsure.dev@gmail.com>
2018-04-19 12:39:13 +0900

    User Domain 생성

---
+++ simpleboard/src/main/java/examples/boot/simpleboard/domain/User.java
@@ -0,0 +1,22 @@
+package examples.boot.simpleboard.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements Serializable { // 객체 직렬화가 가능하도록(Serializable)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime regdate;
+}
--- simpleboard/src/test/java/examples/boot/simpleboard/SimpleboardApplicationTests.java
+++ simpleboard/src/test/java/examples/boot/simpleboard/SimpleboardApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SimpleboardApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
