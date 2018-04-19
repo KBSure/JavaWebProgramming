318b5a1eb731a774ad13dd739711e3d95a52b9a5
KBSure <kbsure.dev@gmail.com>
2018-04-19 14:52:12 +0900

    UserApiController 수정

--- simpleboard/src/main/java/examples/boot/simpleboard/controller/api/UserApiController.java
+++ simpleboard/src/main/java/examples/boot/simpleboard/controller/api/UserApiController.java
@@ -1,7 +1,22 @@
 package examples.boot.simpleboard.controller.api;
 
+import examples.boot.simpleboard.domain.User;
+import examples.boot.simpleboard.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController //Web Api 리턴
+@RequestMapping("/api/users")
 public class UserApiController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
 }
--- simpleboard/src/main/resources/data.sql
+++ simpleboard/src/main/resources/data.sql
@@ -1,5 +1,6 @@
-insert into users(id, name, email, password, regdate) values (1, 'kim sungpark', 'urstory@gmail.com', '1234', now());
-insert into users(id, name, email, password, regdate) values (2, 'kim 2', 'urstory@gmail.com', '1234', now());
-insert into users(id, name, email, password, regdate) values (3, 'kim 3', 'urstory@gmail.com', '1234', now());
-insert into users(id, name, email, password, regdate) values (4, 'kim 4', 'urstory@gmail.com', '1234', now());
-insert into users(id, name, email, password, regdate) values (5, 'kim 5', 'urstory@gmail.com', '1234', now());
+insert into users(id, name, email, password, regdate) values (1, 'kim 1', 'urstory@gmail.com', '1234', now());
+-- insert into users(id, name, email, password, regdate) values (2, 'kim 2', 'urstory@gmail.com', '1234', now());
+-- insert into users(id, name, email, password, regdate) values (3, 'kim 3', 'urstory@gmail.com', '1234', now());
+-- insert into users(id, name, email, password, regdate) values (4, 'kim 4', 'urstory@gmail.com', '1234', now());
+-- insert into users(id, name, email, password, regdate) values (5, 'kim 5', 'urstory@gmail.com', '1234', now());
+-- insert into users(id, name, email, password, regdate) values (6, 'kim 6', 'urstory@gmail.com', '1234', now());
